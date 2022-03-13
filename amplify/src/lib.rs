57961e1193d7b1f1660be4c282c47a6300144869
camercu <cameron.unterberger@gmail.com>
2022-03-12 19:05:39 -0500

    working amplify!

--- amplify/src/lib.rs
+++ amplify/src/lib.rs
@@ -1,19 +1,37 @@
 extern crate proc_macro;
 
-use proc_macro::TokenStream;
-use quote::quote;
-use syn;
+use litrs::StringLit;
+use proc_macro::{Group, Literal, TokenStream, TokenTree};
+
+fn make_uppercase(item: TokenStream) -> TokenStream {
+    item.into_iter()
+        .map(|itm| {
+            match itm {
+                // pass Group items back through recursively
+                TokenTree::Group(grp) => {
+                    let upper = make_uppercase(grp.stream());
+                    let grp = Group::new(grp.delimiter(), upper);
+                    TokenTree::Group(grp)
+                }
+                // modify string literals
+                TokenTree::Literal(lit) => {
+                    if let Ok(strlit) = StringLit::try_from(&lit) {
+                        // Convert string literals to uppercase versions
+                        let upper = strlit.value().to_ascii_uppercase();
+                        TokenTree::Literal(Literal::string(&upper))
+                    } else {
+                        // pass through non-string literals
+                        TokenTree::Literal(lit)
+                    }
+                }
+                // let all other tokens pass through
+                tt => tt,
+            }
+        })
+        .collect()
+}
 
 #[proc_macro_attribute]
 pub fn amplify(_attr: TokenStream, item: TokenStream) -> TokenStream {
-    item
-}
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
+    make_uppercase(item)
 }
--- greet/src/main.rs
+++ greet/src/main.rs
@@ -1,21 +1,25 @@
 use ::clap::Parser;
+use amplify::amplify;
 
+/// A simple command line greeting for demo of Rust.
+#[amplify] // this must be top-most attribute to modify the underlying derive macro
 #[derive(Parser, Debug)]
 #[clap(version)]
 struct Cli {
-    /// who the greeting is for
+    /// who to greet
     name: Option<String>,
-}
-
-fn greet(name: Option<String>) {
-    match name {
-        Some(name) => println!("Hello, {}!", name),
-        None => println!("Hello, World!"),
-    };
 }
 
 fn main() {
     let args = Cli::parse();
 
     greet(args.name);
+}
+
+#[amplify]
+fn greet(name: Option<String>) {
+    match name {
+        Some(name) => println!("Hello, {}!", name),
+        None => println!("Hello, World!"),
+    };
 }
